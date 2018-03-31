c73a6d51fcaa0a74599a6fc7d29f9a6421c2feb4
IMSEMZPZ <IMSEMZPZ@users.noreply.github.com>
2018-03-31 14:36:40 +0800

    matlab file

--- Dual_gap_Different_Block_Different_Algo.m
+++ Dual_gap_Different_Block_Different_Algo.m
@@ -1,13 +1,10 @@
 %   Algorithm:
-%	1: Serial
-%   2: Mini_batch
-%   3: Passcode
-%   4: Cocoa
-%   5: Parallel_SDCA
+%   1: cocoa
+%   2: ppd
 
 
 %% Set Params
-file_name = 'heart_scale';
+file_name = 'kddb.txt';
 
 C = 0.00001;
 
@@ -79,10 +76,10 @@ function [Cocoa_average_dual_gap_32, Cocoa_average_dual_gap_64, Cocoa_average_du
     if(strcmp(file_name, 'heart_scale'))
             file_name = 'heart\_scale';
     end   
-    label = [file_name, ', Cocoa'];
+    label = [file_name, ', CoCoA'];
     title(label);
 end
-function [Cocoa_average_dual_gap_32, Cocoa_average_dual_gap_64, Cocoa_average_dual_gap_128, Cocoa_average_dual_gap_256,  Cocoa_average_dual_gap_512, Cocoa_average_dual_gap_1024] = draw_ad(file_name, C)
+function [Cocoa_add_dual_gap_32, Cocoa_add_dual_gap_64, Cocoa_add_dual_gap_128, Cocoa_add_dual_gap_256,  Cocoa_add_dual_gap_512, Cocoa_add_dual_gap_1024] = draw_ad(file_name, C)
     %%
     loss = 'L2_svm';
     tol = 1e-12;
@@ -96,39 +93,39 @@ function [Cocoa_average_dual_gap_32, Cocoa_average_dual_gap_64, Cocoa_average_du
     algorithm = 'cocoa';
     %% Cocoa-adding
     n_block = 32;
-    [Cocoa_average_primal_val_32, Cocoa_average_dual_val_32, Cocoa_average_dual_gap_32] = ...
+    [Cocoa_add_primal_val_32, Cocoa_add_dual_val_32, Cocoa_add_dual_gap_32] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
     %% Cocoa-adding
     n_block = 64;
-    [Cocoa_average_primal_val_64, Cocoa_average_dual_val_64, Cocoa_average_dual_gap_64] = ...
+    [Cocoa_add_primal_val_64, Cocoa_add_dual_val_64, Cocoa_add_dual_gap_64] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
     %% Cocoa-adding
     n_block = 128;
-    [Cocoa_average_primal_val_128, Cocoa_average_dual_val_128, Cocoa_average_dual_gap_128] = ...
+    [Cocoa_add_primal_val_128, Cocoa_add_dual_val_128, Cocoa_add_dual_gap_128] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
      %% Cocoa-adding
     n_block = 256;
-    [Cocoa_average_primal_val_256, Cocoa_average_dual_val_256, Cocoa_average_dual_gap_256] = ...
+    [Cocoa_add_primal_val_256, Cocoa_add_dual_val_256, Cocoa_add_dual_gap_256] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
     %% Cocoa-adding
     n_block = 512;
-    [Cocoa_average_primal_val_512, Cocoa_average_dual_val_512, Cocoa_average_dual_gap_512] = ...
+    [Cocoa_add_primal_val_512, Cocoa_add_dual_val_512, Cocoa_add_dual_gap_512] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
     n_block = 1024;
-    [Cocoa_average_primal_val_1024, Cocoa_average_dual_val_1024, Cocoa_average_dual_gap_1024] = ...
+    [Cocoa_add_primal_val_1024, Cocoa_add_dual_val_1024, Cocoa_add_dual_gap_1024] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     %% Draw Dual_gap
-    figure,semilogy(0:size(Cocoa_average_dual_gap_32)-1, abs(Cocoa_average_dual_gap_32), 'r-*',...
-        0:size(Cocoa_average_dual_gap_64)-1, abs(Cocoa_average_dual_gap_64), 'g-x',...
-        0:size(Cocoa_average_dual_gap_128)-1, abs(Cocoa_average_dual_gap_128), 'b-s',...
-        0:size(Cocoa_average_dual_gap_256)-1, abs(Cocoa_average_dual_gap_256), 'c-o', ...
-        0:size(Cocoa_average_dual_gap_512)-1, abs(Cocoa_average_dual_gap_512), 'm-+', ...
-        0:size(Cocoa_average_dual_gap_1024)-1, abs(Cocoa_average_dual_gap_1024), 'k-v');
+    figure,semilogy(0:size(Cocoa_add_dual_gap_32)-1, abs(Cocoa_add_dual_gap_32), 'r-*',...
+        0:size(Cocoa_add_dual_gap_64)-1, abs(Cocoa_add_dual_gap_64), 'g-x',...
+        0:size(Cocoa_add_dual_gap_128)-1, abs(Cocoa_add_dual_gap_128), 'b-s',...
+        0:size(Cocoa_add_dual_gap_256)-1, abs(Cocoa_add_dual_gap_256), 'c-o', ...
+        0:size(Cocoa_add_dual_gap_512)-1, abs(Cocoa_add_dual_gap_512), 'm-+', ...
+        0:size(Cocoa_add_dual_gap_1024)-1, abs(Cocoa_add_dual_gap_1024), 'k-v');
     legend('k = 32', 'k = 64', 'k = 128', 'k = 256', 'k = 512','k = 1024');
     xlabel({'Epoch'});
     ylabel({'Dual-Gap'})
@@ -141,12 +138,12 @@ function [Cocoa_average_dual_gap_32, Cocoa_average_dual_gap_64, Cocoa_average_du
     if(strcmp(file_name, 'heart_scale'))
             file_name = 'heart\_scale';
     end   
-    label = [file_name, ', Cocoa +'];
+    label = [file_name, ', CoCoA+'];
     title(label);
 end
 
 
-function [Cocoa_average_dual_gap_32, Cocoa_average_dual_gap_64, Cocoa_average_dual_gap_128, Cocoa_average_dual_gap_256,  Cocoa_average_dual_gap_512, Cocoa_average_dual_gap_1024] = draw_psdca(file_name, C)
+function [PPD_dual_gap_32, PPD_dual_gap_64, PPD_dual_gap_128, PPD_dual_gap_256,  PPD_dual_gap_512, PPD_dual_gap_1024] = draw_psdca(file_name, C)
     %%
     loss = 'L2_svm';
     tol = 1e-12;
@@ -157,42 +154,42 @@ function [Cocoa_average_dual_gap_32, Cocoa_average_dual_gap_64, Cocoa_average_du
     H = 10;
     gamma = 0.333;
     use_best_gamma = 0;
-    algorithm = 'parallel_sdca';
+    algorithm = 'ppd';
     %% Cocoa-adding
     n_block = 32;
-    [Cocoa_average_primal_val_32, Cocoa_average_dual_val_32, Cocoa_average_dual_gap_32] = ...
+    [PPD_primal_val_32, PPD_dual_val_32, PPD_dual_gap_32] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
     %% Cocoa-adding
     n_block = 64;
-    [Cocoa_average_primal_val_64, Cocoa_average_dual_val_64, Cocoa_average_dual_gap_64] = ...
+    [PPD_primal_val_64, PPD_dual_val_64, PPD_dual_gap_64] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
     %% Cocoa-adding
     n_block = 128;
-    [Cocoa_average_primal_val_128, Cocoa_average_dual_val_128, Cocoa_average_dual_gap_128] = ...
+    [PPD_primal_val_128, PPD_dual_val_128, PPD_dual_gap_128] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
      %% Cocoa-adding
     n_block = 256;
-    [Cocoa_average_primal_val_256, Cocoa_average_dual_val_256, Cocoa_average_dual_gap_256] = ...
+    [PPD_primal_val_256, PPD_dual_val_256, PPD_dual_gap_256] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
     %% Cocoa-adding
     n_block = 512;
-    [Cocoa_average_primal_val_512, Cocoa_average_dual_val_512, Cocoa_average_dual_gap_512] = ...
+    [PPD_primal_val_512, PPD_dual_val_512, PPD_dual_gap_512] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     
     n_block = 1024;
-    [Cocoa_average_primal_val_1024, Cocoa_average_dual_val_1024, Cocoa_average_dual_gap_1024] = ...
+    [PPD_primal_val_1024, PPD_dual_val_1024, PPD_dual_gap_1024] = ...
         Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
     %% Draw Dual_gap
-    figure,semilogy(0:size(Cocoa_average_dual_gap_32)-1, abs(Cocoa_average_dual_gap_32), 'r-*',...
-        0:size(Cocoa_average_dual_gap_64)-1, abs(Cocoa_average_dual_gap_64), 'g-x',...
-        0:size(Cocoa_average_dual_gap_128)-1, abs(Cocoa_average_dual_gap_128), 'b-s',...
-        0:size(Cocoa_average_dual_gap_256)-1, abs(Cocoa_average_dual_gap_256), 'c-o', ...
-        0:size(Cocoa_average_dual_gap_512)-1, abs(Cocoa_average_dual_gap_512), 'm-+', ...
-        0:size(Cocoa_average_dual_gap_1024)-1, abs(Cocoa_average_dual_gap_1024), 'k-v');
+    figure,semilogy(0:size(PPD_dual_gap_32)-1, abs(PPD_dual_gap_32), 'r-*',...
+        0:size(PPD_dual_gap_64)-1, abs(PPD_dual_gap_64), 'g-x',...
+        0:size(PPD_dual_gap_128)-1, abs(PPD_dual_gap_128), 'b-s',...
+        0:size(PPD_dual_gap_256)-1, abs(PPD_dual_gap_256), 'c-o', ...
+        0:size(PPD_dual_gap_512)-1, abs(PPD_dual_gap_512), 'm-+', ...
+        0:size(PPD_dual_gap_1024)-1, abs(PPD_dual_gap_1024), 'k-v');
     legend('k = 32', 'k = 64', 'k = 128', 'k = 256', 'k = 512','k = 1024');
     xlabel({'Epoch'});
     ylabel({'Dual-Gap'})
--- Dual_gap_Different_C.m
+++ Dual_gap_Different_C.m
@@ -1,9 +1,6 @@
 %   Algorithm:
-%	1: Serial
-%   2: Mini_batch
-%   3: Passcode
-%   4: Cocoa
-%   5: Parallel_SDCA
+%   1: cocoa
+%   2: ppd
 
 
 %% Set Params
@@ -31,6 +28,7 @@ save './news20_C_00001_dif_alg' a b c
 [a,b,c] = draw(file_name, 0.000001);
 save './news20_C_000001_dif_alg' a b c
 
+
 function [Cocoa_average_dual_gap, Cocoa_add_dual_gap, Parallel_SDCA_dual_gap] = draw(file_name, C)
     %%
     loss = 'L2_svm';
@@ -38,7 +36,6 @@ function [Cocoa_average_dual_gap, Cocoa_add_dual_gap, Parallel_SDCA_dual_gap] = 
     n_epoch = 40;
     verbose = 1;
     n_thread = 32;
-    batch_size = 8;
     n_block = 64;
     H = 10;
     gamma = 0.333;
@@ -46,26 +43,26 @@ function [Cocoa_average_dual_gap, Cocoa_add_dual_gap, Parallel_SDCA_dual_gap] = 
     %% Cocoa-averaging
     algorithm = 'cocoa';
     [Cocoa_average_primal_val, Cocoa_average_dual_val, Cocoa_average_dual_gap] = ...
-        Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
+        Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, n_block, H, gamma, use_best_gamma);
 
     %% Cocoa-adding
     gamma = 1;
     algorithm = 'cocoa';
     [Cocoa_add_primal_val, Cocoa_add_dual_val, Cocoa_add_dual_gap] = ...
-        Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
+        Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, n_block, H, gamma, use_best_gamma);
 
     %% Parallel_SDCA
     gamma = 1;
     use_best_gamma = 1;
-    algorithm = 'parallel_sdca';
+    algorithm = 'ppd';
     [Parallel_SDCA_primal_val, Parallel_SDCA_dual_val, Parallel_SDCA_dual_gap] = ...
-        Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, batch_size, n_block, H, gamma, use_best_gamma);
+        Interface(file_name, loss, algorithm, C, tol, n_epoch, verbose, n_thread, n_block, H, gamma, use_best_gamma);
 
     %% Draw Dual_gap
     figure,semilogy(0:size(Cocoa_average_dual_gap)-1, abs(Cocoa_average_dual_gap), 'r-*',...
         0:size(Cocoa_add_dual_gap)-1, abs(Cocoa_add_dual_gap), 'b-o',...
         0:size(Parallel_SDCA_dual_gap)-1, abs(Parallel_SDCA_dual_gap), 'k-v');
-    legend('Cocoa','Cocoa +','PPD');
+    legend('CoCoA','CoCoA+','PPD');
     xlabel({'Epoch'});
     ylabel({'Dual-Gap'})
     label = ', C = ';
